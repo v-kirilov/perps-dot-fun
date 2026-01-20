20c7cbdd81698a5cf27f4d69d5f1f0d37c5b7540
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-20 20:06:29 +0000

    Add more methods to the data service and update the interfaces.

--- src/app/lib/data-service.ts
+++ src/app/lib/data-service.ts
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
 import { supabase } from "./supabase";
 
 export interface User {
   id: string;
   wallet: string;
+  trades:number;
+  wins:number;
+  losses:number;
+  traded_volume:number;
 }
 
 export interface Trade {
@@ -40,3 +45,33 @@ export const getTrades = async function () {
 
   return data;
 };
+
+export async function getTrade(id: string) {
+  const { data, error } = await supabase
+    .from("trades")
+    .select("id, created_at,trader_id, amount, isOpen")
+    .eq("id", id)
+    .overrideTypes<Trade[], { merge: false }>();
+
+  if (error) {
+    console.error(error);
+    notFound();
+  }
+
+  return data;
+}
+
+export async function getUser(wallet: string) {
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("wallet", wallet)
+    .overrideTypes<User[], { merge: false }>();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Trades could not be loaded");
+  }
+
+  return data;
+}
