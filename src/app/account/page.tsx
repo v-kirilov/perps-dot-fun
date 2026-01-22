ddba19f4d2ab0b8d341d53ebce2fcaccc74ac6c5
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-22 09:11:04 +0000

    Add new component to account page.

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,9 +1,12 @@
+import PPTokenForm from "@/components/PPTokenForm";
+
 export default function Page() {
   return (
     <div className="flex flex-col gap-10 mt-10 items-center">
       <h2 className="text-3xl font-semibold">
         Sign in to access your profile information.
       </h2>
+      <PPTokenForm />
     </div>
   );
 }
