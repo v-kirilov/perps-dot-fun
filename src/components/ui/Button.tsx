5d554c5e3c949c458ac3ae9ccfb9f36e63e74009
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-24 13:34:04 +0000

    Fix buy and sell buttons to take w-full.

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -24,12 +24,12 @@ const ButtonTypes: ButtonType[] = [
   {
     type: "buy",
     className:
-      "text-white bg-gradient-to-br from-green-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2  disabled:bg-gray-500 disabled:cursor-not-allowed disabled:hover:bg-gray-500 w-full",
+      "text-white bg-gradient-to-br from-green-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5  text-center mx-2 my-2  disabled:bg-gray-500 disabled:cursor-not-allowed disabled:hover:bg-gray-500 w-full",
   },
    {
     type: "sell",
     className:
-      "text-white bg-gradient-to-br from-red-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2  disabled:bg-gray-500 disabled:cursor-not-allowed disabled:hover:bg-gray-500 w-full",
+      "text-white bg-gradient-to-br from-red-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-2 my-2  disabled:bg-gray-500 disabled:cursor-not-allowed disabled:hover:bg-gray-500 w-full",
   },
 ];
 
