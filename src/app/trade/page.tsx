9bf76d43b35dc8ec0cc7187fd4935ffd1cb7f5c7
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-25 20:56:55 +0000

    Fetch prices in page.

--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -1,9 +1,13 @@
 import TradingDashboard from "@/components/TradingDashboard";
+import { fetchEthPrice, fetchBtcPrice } from "../lib/price-service";
 
-export default function Home() {
+export default async function Page() {
+  const ethPrice : number = await fetchEthPrice().then();
+  const btcPrice : number = await fetchBtcPrice().then();
+
   return (
     <div className="px-10">
-      <TradingDashboard />
+      <TradingDashboard ethPrice={ethPrice} btcPrice={btcPrice} />
     </div>
   );
 }
