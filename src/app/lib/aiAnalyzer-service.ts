d102b6affacf339ff5a8472329cce43227d5dec8
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-26 22:36:27 +0000

    Fine tune the instructions for GPT

--- src/app/lib/aiAnalyzer-service.ts
+++ src/app/lib/aiAnalyzer-service.ts
@@ -3,19 +3,17 @@
 
 import OpenAI from "openai";
 
-export const askGPT = async function () {
-   // process.loadEnvFile;
-  //  process.env['OPENAI_API_KEY']
-  console.log("API KEY:", process.env.OPENAI_API_KEY);
+export const askGPT = async function (input: string): Promise<string> {
   const client: any = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY, 
+    apiKey: process.env.PERPS_FUN_OAI_KEY, 
   });
 
   const response = await client.responses.create({
-    model: "gpt-4o",
-    instructions: "You are a coding assistant that talks like a pirate",
-    input: "Are semicolons optional in JavaScript?",
+    model: "gpt-5-nano",
+    instructions: "You are a trader and a financial expert. I want you to give me your outlook on the market based on the following data I provide in the input. Also make sure to format the output as a JSON object with the following fields: 'summary', 'trend', 'key_levels', and 'recommendation'. Be concise and to the point.",
+    input: input,
   });
 
   console.log(response.output_text);
+  return response.output_text;
 };
