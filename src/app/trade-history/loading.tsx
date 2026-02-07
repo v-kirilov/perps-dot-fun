cff16c4ee55f73678754a9d959dac8bbb17b48d9
v-kirilov <v_kirilov87@protonmail.ch>
2026-02-07 08:03:29 +0000

    Add changes to loading screen.

--- src/app/trade-history/loading.tsx
+++ src/app/trade-history/loading.tsx
@@ -2,9 +2,9 @@ import Spinner from "@/components/Spinner";
 
 export default function Loading() {
   return (
-    <div className="grid items-center justify-center">
-      <Spinner />
-      <span>Fetching account data...</span>
+    <div className="flex items-center justify-center flex-col space-y-2 h-screen">
+      <Spinner/>
+      <span className="text-xl font-semibold text-gray-800">Fetching account data...</span>
     </div>
   );
 }
