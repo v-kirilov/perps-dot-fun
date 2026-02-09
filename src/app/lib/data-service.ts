d2d2596907a7d6ff22e0eb245d13e655ca13a01c
v-kirilov <v_kirilov87@protonmail.ch>
2026-02-09 19:54:45 +0000

    Extend `Trade` interface , add functionality to close trades and update database. Rework `openTrade` function.

--- src/app/lib/data-service.ts
+++ src/app/lib/data-service.ts
@@ -1,6 +1,7 @@
 "use server";
 import { notFound } from "next/navigation";
 import { supabase } from "./supabase";
+import { getETHPrice } from "./binance-service";
 
 export interface User {
   id?: string;
@@ -19,6 +20,10 @@ export interface Trade {
   amount: number;
   isOpen: boolean;
   profit: number;
+  margin: number;
+  entry_price: number;
+  closing_price?: number;
+  eth_amount: number;
 }
 
 export const getUsers = async function () {
@@ -38,7 +43,8 @@ export const getUsers = async function () {
 export const getTrades = async function () {
   const { data, error } = await supabase
     .from("trades")
-    .select("id, created_at, closed_at, trader_id, amount, isOpen, profit")
+    .select("id, created_at, closed_at, trader_id, amount, isOpen, profit, eth_amount, entry_price")
+    .order("id", { ascending: false })
     .overrideTypes<Trade[], { merge: false }>();
 
   if (error) {
@@ -52,16 +58,16 @@ export const getTrades = async function () {
 export async function getTrade(id: string) {
   const { data, error } = await supabase
     .from("trades")
-    .select("id, created_at, closed_at, trader_id, amount, isOpen, profit")
+    .select("id, created_at, closed_at, trader_id, amount, isOpen, profit, margin, entry_price, closing_price, eth_amount, entry_price") 
     .eq("id", id)
-    .overrideTypes<Trade[], { merge: false }>();
+    .single();
 
   if (error) {
     console.error(error);
     notFound();
   }
 
-  return data;
+  return data as Trade;
 }
 
 export async function getUser(wallet: string) {
@@ -87,7 +93,6 @@ export async function getOrCreateUser(wallet: string) {
     .select("*")
     .eq("wallet", wallet)
     .overrideTypes<User[], { merge: false }>();
- 
 
   if (fetchError) {
     throw new Error("Could not fetch user");
@@ -99,14 +104,13 @@ export async function getOrCreateUser(wallet: string) {
   }
 
   // Create new user with default values
-  const newUser:User = {
+  const newUser: User = {
     wallet,
     trades: 0,
     wins: 0,
     losses: 0,
     traded_volume: 0,
   };
-
 
   const { data: createdUser, error: insertError } = await supabase
     .from("users")
@@ -121,25 +125,57 @@ export async function getOrCreateUser(wallet: string) {
   return createdUser;
 }
 
-export async function openTrade(trader_id: number, amount: number, openedDate:Date) {
-  console.log(
-    "Opening trade for trader_id:",
-    trader_id,
-    "with amount:",
-    amount,
-  );
+export async function openTrade(
+  trader_id: number,
+  amount: number,
+  openedDate: Date,
+  margin: number,
+  ethAmount: number,
+) {
+  const currentPrice = await getETHPrice("ETHUSDT");
+
   const trade: Trade = {
     created_at: openedDate,
     trader_id: trader_id,
     amount: amount,
     isOpen: true,
     profit: 0,
+    margin: margin,
+    entry_price: currentPrice,
+    eth_amount: ethAmount,
   };
   const { data, error } = await supabase.from("trades").insert([trade]);
 
   if (error) {
     console.error("Error inserting trade:", error);
     throw new Error("Trades could not be opened");
+  }
+  return data;
+}
+
+export async function closeTrade(tradeId: string, closeDate: Date) {
+  const trade:Trade = await getTrade(tradeId.toString());
+
+  const currentPrice = await getETHPrice("ETHUSDT");
+  console.log(`Current trade is:`, trade);
+  
+  // Calculate profit based on entry price and current price
+  const profit = (currentPrice - trade.entry_price) * trade.eth_amount * trade.margin;
+  console.log(`Current price for trade ${tradeId}:`, currentPrice, trade.entry_price ,trade.eth_amount, trade.margin);
+  const { data, error } = await supabase
+    .from("trades")
+    .update({
+      closed_at: closeDate,
+      isOpen: false,
+      profit: profit,
+    })
+    .eq("id", tradeId)
+    .select();
+
+  console.log("Updated trade data:", data);
+  if (error) {
+    console.error("Error closing trade:", error);
+    throw new Error("Trades could not be closed");
   }
   return data;
 }
